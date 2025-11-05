26ea0a78fe1e3f8659490b05eef6bf3d5a5b89bc
Ngô Minh Thuận <thuanthichlaptrinh@gmail.com>
2025-11-06 01:27:43 +0700

    feat: cấu hình Docker cho dự án

--- src/main/java/com/thuanthichlaptrinh/card_words/configuration/SecurityConfiguration.java
+++ src/main/java/com/thuanthichlaptrinh/card_words/configuration/SecurityConfiguration.java
@@ -26,82 +26,83 @@ import lombok.RequiredArgsConstructor;
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
-    private static final String[] WHITE_LIST_URL = {
-            "/login/oauth2/**",
-            "/api/v1/auth/signin",
-            "/api/v1/auth/signup",
-            "/api/v1/auth/google",
-            "/api/v1/auth/forgot-password",
-            "/api/v1/auth/refresh-token",
-            "/favicon.ico",
-            "/static/**",
-            "/css/**",
-            "/js/**",
-            "/images/**",
-            "/webjars/**",
-            "/v2/api-docs",
-            "/v3/api-docs",
-            "/v3/api-docs/**",
-            "/swagger-resources",
-            "/swagger-resources/**",
-            "/configuration/ui",
-            "/configuration/security",
-            "/swagger-ui/**",
-            "/swagger-ui.html",
-            "/swagger-ui.html/**",
-            "/swagger-ui/index.html/**",
-            "/api-test.html",
-            "/google-test.html",
-            "https://accounts.google.com/signin/oauth2/**",
-            "https://developers.google.com/oauthplayground/**"
-    };
-    private final JwtAuthenticationFilter jwtAuthFilter;
-    private final AuthenticationProvider authenticationProvider;
+        private static final String[] WHITE_LIST_URL = {
+                        "/login/oauth2/**",
+                        "/api/v1/auth/signin",
+                        "/api/v1/auth/signup",
+                        "/api/v1/auth/google",
+                        "/api/v1/auth/forgot-password",
+                        "/api/v1/auth/refresh-token",
+                        "/favicon.ico",
+                        "/static/**",
+                        "/css/**",
+                        "/js/**",
+                        "/images/**",
+                        "/webjars/**",
+                        "/v2/api-docs",
+                        "/v3/api-docs",
+                        "/v3/api-docs/**",
+                        "/swagger-resources",
+                        "/swagger-resources/**",
+                        "/configuration/ui",
+                        "/configuration/security",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html",
+                        "/swagger-ui.html/**",
+                        "/swagger-ui/index.html/**",
+                        "/api-test.html",
+                        "/google-test.html",
+                        "/actuator/**",
+                        "https://accounts.google.com/signin/oauth2/**",
+                        "https://developers.google.com/oauthplayground/**"
+        };
+        private final JwtAuthenticationFilter jwtAuthFilter;
+        private final AuthenticationProvider authenticationProvider;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(req -> req.requestMatchers(WHITE_LIST_URL).permitAll()
-                        .anyRequest()
-                        .authenticated())
-                .cors(cors -> cors.configurationSource(
-                        request -> {
-                            CorsConfiguration configuration = new CorsConfiguration();
-                            configuration.setAllowedOrigins(List.of("*"));
-                            configuration.setAllowedMethods(List.of("*"));
-                            configuration.setAllowedHeaders(List.of("*"));
-                            return configuration;
-                        }))
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling(exceptionHandling -> exceptionHandling
-                        .authenticationEntryPoint((request, response, authException) -> {
-                            response.setStatus(HttpStatus.FORBIDDEN.value());
-                            response.setContentType("application/json");
-                            response.getWriter()
-                                    .write("{\"status\":\"403\",\"message\":\"Access Denied\",\"data\":null}");
-                        })
-                        .accessDeniedHandler((request, response, accessDeniedException) -> {
-                            response.setStatus(HttpStatus.FORBIDDEN.value());
-                            response.setContentType("application/json");
-                            response.getWriter()
-                                    .write("{\"status\":\"403\",\"message\":\"Access Denied\",\"data\":null}");
-                        }))
-                .logout(AbstractHttpConfigurer::disable);
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http.csrf(AbstractHttpConfigurer::disable)
+                                .authorizeHttpRequests(req -> req.requestMatchers(WHITE_LIST_URL).permitAll()
+                                                .anyRequest()
+                                                .authenticated())
+                                .cors(cors -> cors.configurationSource(
+                                                request -> {
+                                                        CorsConfiguration configuration = new CorsConfiguration();
+                                                        configuration.setAllowedOrigins(List.of("*"));
+                                                        configuration.setAllowedMethods(List.of("*"));
+                                                        configuration.setAllowedHeaders(List.of("*"));
+                                                        return configuration;
+                                                }))
+                                .sessionManagement(session -> session
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .authenticationProvider(authenticationProvider)
+                                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                                .exceptionHandling(exceptionHandling -> exceptionHandling
+                                                .authenticationEntryPoint((request, response, authException) -> {
+                                                        response.setStatus(HttpStatus.FORBIDDEN.value());
+                                                        response.setContentType("application/json");
+                                                        response.getWriter()
+                                                                        .write("{\"status\":\"403\",\"message\":\"Access Denied\",\"data\":null}");
+                                                })
+                                                .accessDeniedHandler((request, response, accessDeniedException) -> {
+                                                        response.setStatus(HttpStatus.FORBIDDEN.value());
+                                                        response.setContentType("application/json");
+                                                        response.getWriter()
+                                                                        .write("{\"status\":\"403\",\"message\":\"Access Denied\",\"data\":null}");
+                                                }))
+                                .logout(AbstractHttpConfigurer::disable);
 
-        return http.build();
-    }
+                return http.build();
+        }
 
-    @Bean
-    public CorsFilter corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+        @Bean
+        public CorsFilter corsFilter() {
+                final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                final CorsConfiguration config = new CorsConfiguration();
+                config.addAllowedOrigin("*");
+                config.addAllowedHeader("*");
+                config.addAllowedMethod("*");
+                source.registerCorsConfiguration("/**", config);
+                return new CorsFilter(source);
+        }
 }
